aa71877048dc283a6b9274f6a87f3a50f487ff10
SANDHYA <115525118+sandhyabalamurali@users.noreply.github.com>
2024-03-11 09:48:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/lifecyclemethods/MainActivity.java
+++ app/src/main/java/com/example/lifecyclemethods/MainActivity.java
@@ -1,14 +1,61 @@
-package com.example.lifecyclemethods;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.sann;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(), "Application Opened", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onStart()
+    {
+        // It will show a message on the screen
+        // then onStart is invoked
+        super.onStart();
+        Toast toast = Toast.makeText(getApplicationContext(), "Application Started", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onRestart() {
+        // It will show a message on the screen
+        // then onRestart is invoked
+        super.onRestart();
+        Toast toast = Toast.makeText(getApplicationContext(), "Restarted", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onResume() {
+        // It will show a message on the screen
+        // then onResume is invoked
+        super.onResume();
+        Toast toast = Toast.makeText(getApplicationContext(), "Application Resumed", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onPause() {
+        // It will show a message on the screen
+        // then onPause is invoked
+        super.onPause();
+        Toast toast = Toast.makeText(getApplicationContext(), "Application Paused", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onStop() {
+        // It will show a message on the screen
+        // then onStop is invoked
+        super.onStop();
+        Toast toast = Toast.makeText(getApplicationContext(), "On Soped", Toast.LENGTH_LONG);
+        toast.show();
+    }
+    protected void onDestroy() {
+        // It will show a message on the screen
+        // then onDestroy is invoked
+        super.onDestroy();
+        Toast toast = Toast.makeText(getApplicationContext(), "onDestroy Called", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
